67bc1179c2df8ff8e104c45b8b139eac742d6ca7
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-04 00:38:09 +0700

    Update JenkaProvider.kt

--- JenkaProvider/src/main/kotlin/com/lagradost/JenkaProvider.kt
+++ JenkaProvider/src/main/kotlin/com/lagradost/JenkaProvider.kt
@@ -1,18 +1,35 @@
 package com.lagradost
 
+import android.util.Log
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.APIHolder.getCaptchaToken
+import com.lagradost.cloudstream3.APIHolder.unixTimeMS
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.LoadResponse.Companion.addDuration
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
-import com.lagradost.cloudstream3.mvvm.safeApiCall
-import com.lagradost.cloudstream3.utils.*
+//import com.lagradost.cloudstream3.animeproviders.ZoroProvider
+import com.lagradost.cloudstream3.mvvm.suspendSafeApiCall
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.Coroutines.ioSafe
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.M3u8Helper
+import com.lagradost.cloudstream3.utils.getQualityFromName
+import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.nicehttp.NiceResponse
+import kotlinx.coroutines.delay
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
-import java.net.URLDecoder
-import java.util.ArrayList
+import java.net.URI
+import java.util.*
+import kotlin.system.measureTimeMillis
 
-class JenkaProvider : MainAPI() {
+open class JenkaProvider : MainAPI() {
     override var mainUrl = "https://www.jenkastudiovn.net"
     override var name = "JenkaStudioVN"
+    
     override val hasMainPage = true
     override var lang = "vi"
     override val hasDownloadSupport = true
@@ -20,21 +37,34 @@ class JenkaProvider : MainAPI() {
         TvType.Movie,
         TvType.TvSeries,
         TvType.Anime,
-        TvType.AsianDrama
     )
+    
+    override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse {
+        val html = app.get("$mainUrl").text
+        val document = Jsoup.parse(html)
 
-    override val mainPage = mainPageOf(
-        "$mainUrl/search/label/phim-hot?&max-results=48" to "Phim Hot",
-        "$mainUrl/search/label/phim-le?&max-results=48" to "Phim Lẻ",
-        "$mainUrl/search/label/ghibli-collection?&max-results=48" to "Phim Ghibli",
-    )
-    
-    override suspend fun getMainPage(
-        page: Int,
-        request: MainPageRequest
-    ): HomePageResponse {
-        val document = app.get(request.data + page).document ?: return@mapNotNull null
+        val all = ArrayList<HomePageList>()
+
+        val map = mapOf(
+            "Phim lẻ" to "div#LinkList1_content_0.tab-content",
+            "Phim bộ" to "div#LinkList1_content_1.tab-content",            
+        )
+        map.forEach {
+            all.add(HomePageList(
+                it.key,
+                document.select(it.value).select("div.phimitem").map { element ->
+                    element.toSearchResult()
+                }
+            ))
+        }
+
+        document.select("div.owl-stage").forEach {
+            val title = it.select("h3.lable-home").text().trim()
+            val elements = it.select("div.phimitem").map { element ->
+                element.toSearchResult()
+            }
+            all.add(HomePageList(title, elements))
+        }
+
+        return HomePageResponse(all)
     }
-    
-     private fun decode(input: String): String? = URLDecoder.decode(input, "utf-8")
-}
