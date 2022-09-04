b3cf11a1d6cb5307b73d5917865ee6cd645c3e0e
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-04 10:04:26 +0700

    Update PhimmoichillProvider.kt

--- PhimmoichillProvider/src/main/kotlin/com/lagradost/PhimmoichillProvider.kt
+++ PhimmoichillProvider/src/main/kotlin/com/lagradost/PhimmoichillProvider.kt
@@ -97,9 +97,9 @@ class PhimmoichillProvider : MainAPI() {
             document.select("ul.entry-meta.block-film li:nth-child(7) span").text().toRatingInt()
         val actors = document.select("ul.entry-meta.block-film li:last-child a").map { it.text() }
         val recommendations = document.select("ul#list-film-realted li.item").mapNotNull {
-                val titleHeader = it.select("p") ?: return@mapNotNull null
+                val titleHeader = it.select("a") ?: return@mapNotNull null
                 val recUrl = titleHeader.attr("href") ?: return@mapNotNull null
-                val recTitle = titleHeader.text() ?: return@mapNotNull null
+                val recTitle = titleHeader.select("p").text() ?: return@mapNotNull null
                 val poster = titleHeader.select("img.lazyloaded").attr("src")
                 MovieSearchResponse(
                     recTitle,
