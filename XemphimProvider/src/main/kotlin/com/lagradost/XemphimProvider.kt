b2250cd86fc96ef9e2fb365100d5aabf361ebfe0
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-05 18:05:43 +0700

    Update XemphimProvider.kt

--- XemphimProvider/src/main/kotlin/com/lagradost/XemphimProvider.kt
+++ XemphimProvider/src/main/kotlin/com/lagradost/XemphimProvider.kt
@@ -39,7 +39,6 @@ open class XemphimProvider : MainAPI() {
         TvType.Movie,
         TvType.TvSeries,
     )
-    override val vpnStatus = VPNStatus.cloudflare
 
     override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse {
         val html = app.get("$mainUrl").text
