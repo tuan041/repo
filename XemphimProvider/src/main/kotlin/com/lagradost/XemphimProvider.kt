08d601bf175ede185f2d26dfdca87c5cadfb6e02
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-06 23:13:03 +0700

    Update XemphimProvider.kt

--- XemphimProvider/src/main/kotlin/com/lagradost/XemphimProvider.kt
+++ XemphimProvider/src/main/kotlin/com/lagradost/XemphimProvider.kt
@@ -73,8 +73,8 @@ class XemphimProvider : MainAPI() {
 
         return document.select("div.item.col-lg-2.col-md-3.col-sm-4.col-6").mapNotNull {
                 val main = it.select("div.item.col-lg-2.col-md-3.col-sm-4.col-6") ?: return@mapNotNull null
-                val titleHeader = if (main.select("p:nth-child(1)").isNotEmpty())
-                    main.select("p:nth-child(1)") else main.select("p.subtitle:nth-child(2)") ?: return@mapNotNull null
+                val titleHeader = if (main.select("p:first-child").isNotEmpty())
+                    main.select("p:first-child") else main.select("p:last-child") ?: return@mapNotNull null
                 val recUrl = it.select("a").attr("href") ?: return@mapNotNull null
                 val recTitle = titleHeader.text() ?: return@mapNotNull null
                 val poster = main.select("img").attr("src") ?: return@mapNotNull null
