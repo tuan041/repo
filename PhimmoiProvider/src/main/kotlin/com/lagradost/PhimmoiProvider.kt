fbb63ca87db3e3807fee8649b86ad3c4e8175aeb
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-07 12:46:09 +0700

    Update PhimmoiProvider.kt

--- PhimmoiProvider/src/main/kotlin/com/lagradost/PhimmoiProvider.kt
+++ PhimmoiProvider/src/main/kotlin/com/lagradost/PhimmoiProvider.kt
@@ -89,25 +89,24 @@ class PhimmoiProvider : MainAPI() {
             .toIntOrNull()
         val tvType = if (document.select("div.latest-episode").isNotEmpty()
         ) TvType.TvSeries else TvType.Movie
-        val description = document.select("div#film-content").text().trim()
-                ?.substringBefore("@phimmoi")
+        val description = document.select("div#film-content").text().trim().substringBefore("@phimmoi")
         val trailer =
             document.select("div#trailer script").last()?.data()?.substringAfter("file: \"")
                 ?.substringBefore("\",")
         val rating =
-            document.select("ul.entry-meta.block-film li:nth-child(7) span.imdb").text().toRatingInt()
+            document.select("ul.entry-meta.block-film li:nth-child(7)").text().toRatingInt().substringAfter("IMDb:")
         val actors = document.select("ul.entry-meta.block-film li:last-child a").map { it.text() }
         val recommendations = document.select("ul#list-film-realted li.item").mapNotNull {
                 val main = it.select("ul#list-film-realted li.item")
                 val recUrl = it.select("a").attr("href")
                 val recTitle = it.select("p").text()
-                val poster = decode(main.select("a > img")!!.attr("data-src").substringAfter("url="))
+                val posterUrl = decode(main.select("a > img.lazyloaded").attr("data-src").substringAfter("url="))
                 MovieSearchResponse(
                     recTitle,
                     recUrl,
                     this.name,
                     TvType.Movie,
-                    poster,
+                    poster = posterUrl,
                 )
         }
 
