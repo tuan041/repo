fbbd641978e9096f325399e78b43643b642fdf85
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-14 23:05:29 +0700

    Create build.gradle.kts

---
+++ Phim1080Provider/build.gradle.kts
@@ -0,0 +1,26 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "vi"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Bản thử nghiệm"
+    authors = listOf("TuaSan")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://xemphim1080.com&sz=%size%"
+}
