f5f086ef90d0cc32d00de49aa76f66f7b73458be
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-15 09:36:33 +0700

    Update build.gradle.kts

--- Phim1080Provider/build.gradle.kts
+++ Phim1080Provider/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
         "Movie",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=https://xemphim1080.com&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://phimnhua.com&sz=%size%"
 }
