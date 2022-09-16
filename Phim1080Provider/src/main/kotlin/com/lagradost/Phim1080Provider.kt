30831bcea87519556b5c1d4ea169dbbc302f05b0
tuan041 <30403510+tuan041@users.noreply.github.com>
2022-09-16 12:33:00 +0700

    Update Phim1080Provider.kt

--- Phim1080Provider/src/main/kotlin/com/lagradost/Phim1080Provider.kt
+++ Phim1080Provider/src/main/kotlin/com/lagradost/Phim1080Provider.kt
@@ -93,8 +93,8 @@ class PhimnhuaProvider : MainAPI() {
         ) TvType.TvSeries else TvType.Movie
         val description = document.select("div.col-sm-6.col-md-8.col-lg-9.col-xl-7 > div.card__content > ul.card__meta > li:last-child > div > p").text().trim()
         val episodes = document.select("ul.list.list-inline.justify-content-center > li.list-inline-item").map {
-            val name = it.selectFirst("button")?.text()
-            val link = it.selectFirst("button")?.attr("data-url")
+            val name = it.selectFirst("button").text()
+            val link = it.selectFirst("button").attr("data-url")
             Episode(link, name)
         }.reversed()
         
